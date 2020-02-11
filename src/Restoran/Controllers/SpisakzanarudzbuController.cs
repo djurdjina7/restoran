7ebc5a860a13e42b643990d871dfc4accbbd29f1
Djina <djina.tepic7@gmail.com>
2020-02-11 12:06:08 +0100

    Edit SpisakZaNarudzbu controller.

--- src/Restoran/Controllers/SpisakzanarudzbuController.cs
+++ src/Restoran/Controllers/SpisakzanarudzbuController.cs
@@ -18,10 +18,26 @@ namespace Restoran.Controllers
             _context = context;
         }
 
-        // GET: Spisakzanarudzbu
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? narudzbaId)
         {
-            var restoranContext = _context.Spisakzanarudzbu.Include(s => s.Meni).Include(s => s.Narudzba);
+            if(narudzbaId == 0)
+            {
+                return NotFound();
+            }
+
+            var narudzba = await _context.Narudzba.FindAsync(narudzbaId);
+            if (narudzba == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Cijena = narudzba.Cijena;
+            ViewBag.NarudzbaId = narudzbaId;
+            ViewBag.Datum = narudzba.VrijemeKreiranja;
+
+            var restoranContext = _context.Spisakzanarudzbu
+                .Include(s => s.Meni)
+                .Include(s => s.Narudzba)
+                .Where(x => x.NarudzbaId == narudzbaId);
             return View(await restoranContext.ToListAsync());
         }
 
@@ -31,8 +47,12 @@ namespace Restoran.Controllers
                 .Include(k => k.Meni)
                 .Include(k => k.Narudzba)
                 .Where(x => x.NarudzbaId == narudzbaId);
+
             ViewBag.NarudzbaId = narudzbaId;
 
+            var brojElemenata = _context.Spisakzanarudzbu.Where(r => r.NarudzbaId == narudzbaId).Count();
+
+            ViewBag.brojElemenata = brojElemenata;
 
             var narudzba = await _context.Narudzba.FindAsync(narudzbaId);
             if (narudzba == null)
@@ -43,7 +63,6 @@ namespace Restoran.Controllers
             return View(await restoranContext.ToListAsync());
         }
 
-        // GET: Spisakzanarudzbu/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -63,45 +82,47 @@ namespace Restoran.Controllers
             return View(spisakzanarudzbu);
         }
 
-        // GET: Spisakzanarudzbu/Create
         public IActionResult Create(int? narudzbaId)
         {
-            ViewData["MeniId"] = new SelectList(_context.Meni, "Id", "Naziv");
+            var meni = _context.Meni
+                .Select(x => new
+                {
+                    Id = x.Id,
+                    Podaci = x.TipMenija.Naziv.ToString() + " - " + x.Naziv.ToString()
+                });
+            ViewData["MeniId"] = new SelectList(meni, "Id", "Podaci");
             ViewBag.NarudzbaId = narudzbaId;
             return View();
         }
 
-        // POST: Spisakzanarudzbu/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(int narudzbaId, [Bind("MeniId,NarudzbaId,Kolicina")] Spisakzanarudzbu spisakzanarudzbu)
         {
             if (ModelState.IsValid)
             {
-                var staraNarudzba = await _context.Spisakzanarudzbu.FindAsync(spisakzanarudzbu.MeniId, narudzbaId);
+                var stariSpisakZaNarudzbu = await _context.Spisakzanarudzbu.FindAsync(narudzbaId, spisakzanarudzbu.MeniId);
                 var meni = await _context.Meni.FindAsync(spisakzanarudzbu.MeniId);
                 var narudzba = await _context.Narudzba.FindAsync(narudzbaId);
-                
 
-                if(staraNarudzba != null)
+                decimal novaCijena = (decimal)(spisakzanarudzbu.Kolicina * meni.Cijena);
+
+                if (stariSpisakZaNarudzbu != null)
                 {
                     if(meni.Kolicina > spisakzanarudzbu.Kolicina)
                     {
-                        decimal cijena = (decimal) (spisakzanarudzbu.Kolicina * meni.Cijena);
-                        staraNarudzba.Cijena += cijena;
-                        staraNarudzba.Kolicina += spisakzanarudzbu.Kolicina;
+                        stariSpisakZaNarudzbu.Cijena += novaCijena;
+                        stariSpisakZaNarudzbu.Kolicina += spisakzanarudzbu.Kolicina;
+                        _context.Update(stariSpisakZaNarudzbu);
 
-                        narudzba.Cijena += cijena;
+                        narudzba.Cijena += novaCijena;
                         _context.Update(narudzba);
-                        _context.Update(staraNarudzba);
 
                         meni.Kolicina -= spisakzanarudzbu.Kolicina;
                         _context.Update(meni);
 
                         await _context.SaveChangesAsync();
-                        return RedirectToAction("Cart", "Spisakzanarudzbu", new { narudzbaId = staraNarudzba.NarudzbaId});
+                        return RedirectToAction("Cart", "Spisakzanarudzbu", new { narudzbaId = stariSpisakZaNarudzbu.NarudzbaId});
                     }
                     else
                     {
@@ -110,12 +131,11 @@ namespace Restoran.Controllers
                 }
                 else
                 {
-                    decimal cijena = (decimal)(spisakzanarudzbu.Kolicina * meni.Cijena);
-                    spisakzanarudzbu.Cijena += cijena;
-
-                    narudzba.Cijena += cijena;
-                    _context.Update(narudzba);
+                    spisakzanarudzbu.Cijena = novaCijena;
                     _context.Add(spisakzanarudzbu);
+
+                    narudzba.Cijena += novaCijena;
+                    _context.Update(narudzba);
 
                     meni.Kolicina -= spisakzanarudzbu.Kolicina;
                     _context.Update(meni);
@@ -128,7 +148,74 @@ namespace Restoran.Controllers
             return View(spisakzanarudzbu);
         }
 
-        // GET: Spisakzanarudzbu/Edit/5
+        public async Task<IActionResult> RemoveNarudzba(int narudzbaId, int meniId)
+        {
+            var spisakZaNarudzbu = await _context.Spisakzanarudzbu.FindAsync(narudzbaId, meniId);
+            var meni = await _context.Meni.FindAsync(meniId);
+            var narudzba = await _context.Narudzba.FindAsync(narudzbaId);
+
+            meni.Kolicina += spisakZaNarudzbu.Kolicina;
+            _context.Update(meni);
+
+            narudzba.Cijena -= spisakZaNarudzbu.Cijena;
+            _context.Update(narudzba);
+
+            _context.Spisakzanarudzbu.Remove(spisakZaNarudzbu);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Cart", "Spisakzanarudzbu", new { narudzbaId = spisakZaNarudzbu.NarudzbaId });
+        }
+
+        public async Task<ActionResult> Discard(int? narudzbaId)
+        {
+            if (narudzbaId == null)
+            {
+                return NotFound();
+            }
+
+            var spisakZaNarudzbu = await _context.Spisakzanarudzbu
+                .Include(k => k.Meni)
+                .Where(m => m.NarudzbaId == narudzbaId)
+                .ToListAsync();
+            if (spisakZaNarudzbu == null)
+            {
+                return NotFound();
+            }
+
+            foreach (var item in spisakZaNarudzbu)
+            {
+                var jednaNarudzba = await _context.Spisakzanarudzbu.FindAsync(narudzbaId, item.MeniId);
+                var meni = await _context.Meni.FindAsync(item.MeniId);
+
+                meni.Kolicina += jednaNarudzba.Kolicina;
+                _context.Update(meni);
+
+                _context.Spisakzanarudzbu.Remove(jednaNarudzba);
+            }
+
+            var narudzba = await _context.Narudzba.FindAsync(narudzbaId);
+
+            var sto = await _context.Sto.FindAsync(narudzba.StoId);
+            sto.Dostupan = 1;
+
+            _context.Update(sto);
+            _context.Remove(narudzba);
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Narudzba");
+        }
+
+        public async Task<IActionResult> Finish(int narudzbaId)
+        {
+            var narudzba = await _context.Narudzba.FindAsync(narudzbaId);
+
+
+            narudzba.VrijemeZavrsetka = DateTime.Now;
+            _context.Update(narudzba);
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Narudzba");
+        }
+
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -146,9 +233,6 @@ namespace Restoran.Controllers
             return View(spisakzanarudzbu);
         }
 
-        // POST: Spisakzanarudzbu/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("MeniId,NarudzbaId,Kolicina,Cijena")] Spisakzanarudzbu spisakzanarudzbu)
@@ -183,7 +267,6 @@ namespace Restoran.Controllers
             return View(spisakzanarudzbu);
         }
 
-        // GET: Spisakzanarudzbu/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -203,7 +286,6 @@ namespace Restoran.Controllers
             return View(spisakzanarudzbu);
         }
 
-        // POST: Spisakzanarudzbu/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
